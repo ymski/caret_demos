83633ec1b220bfab7afc40223b1b82e858876110
hsgwa <hasegawa.ats@gmail.com>
2021-08-21 03:41:58 +0900

    add: talker_listener.launch.py
    Signed-off-by: hsgwa <hasegawa.ats@gmail.com>

---
+++ launch/talker_listener.launch.py
@@ -0,0 +1,19 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from tracetools_launch.action import Trace
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        Trace(
+            session_name='talker_listener',
+            events_kernel=[''],
+            events_ust=['ros2*']
+        ),
+        launch_ros.actions.Node(
+            package='demo_nodes_cpp', executable='talker', output='screen'),
+        launch_ros.actions.Node(
+            package='demo_nodes_cpp', executable='listener', output='screen'),
+    ])
