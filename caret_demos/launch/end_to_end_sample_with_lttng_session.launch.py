048fa2e7dccef78d34fc79ea2db82e85918853f2
hsgwa <hasegawa.ats@gmail.com>
2021-12-20 18:43:47 +0900

    add: with_lttng_session.launch

--- caret_demos/launch/end_to_end_sample_with_lttng_session.launch.py
+++ caret_demos/launch/end_to_end_sample_with_lttng_session.launch.py
@@ -2,10 +2,16 @@ import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
+from tracetools_launch.action import Trace
 
 
 def generate_launch_description():
     return launch.LaunchDescription([
+        Trace(
+            session_name='end_to_end_sample',
+            events_kernel=[],
+            events_ust=['ros2*']
+        ),
         launch_ros.actions.Node(
             package='caret_demos', executable='end_to_end_sample', output='screen'),
     ])
