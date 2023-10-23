1fc6f5fedaf60b2ecd2afacd5d8845f81df6961a
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2023-10-23 16:56:25 +0900

    chore: add use_sim_time adn use_rosbag params (#7)
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- caret_demos/launch/end_to_end_sample.launch.py
+++ caret_demos/launch/end_to_end_sample.launch.py
@@ -5,7 +5,10 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='caret_demos', executable='end_to_end_sample', output='screen'),
-    ])
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
+    use_rosbag = launch.substitutions.LaunchConfiguration('use_rosbag', default='false')
+    return launch.LaunchDescription(
+        [launch_ros.actions.Node(
+            package='caret_demos', executable='end_to_end_sample', output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'use_rosbag': use_rosbag}]),])
